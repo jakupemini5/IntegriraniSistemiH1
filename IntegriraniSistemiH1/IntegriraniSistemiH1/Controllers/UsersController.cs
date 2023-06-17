e28dd0dfc2e205d15731817fc2fa0aaaadf93157
Jakup Emini <jakupknio@gmail.com>
2023-06-17 19:26:29 +0200

    added import users front-end

---
+++ IntegriraniSistemiH1/IntegriraniSistemiH1/Controllers/UsersController.cs
@@ -0,0 +1,82 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using IntegriraniSistemiH1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Text;
+
+namespace IntegriraniSistemiH1.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // GET: UsersController
+        public ActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+
+        // GET: UsersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public FileResult DownloadCSV()
+        {
+            // Generate the CSV content
+            var csvContent = "UserName,Password\n";
+            csvContent += "test@gmail.com,TestPassword\n";
+
+            // Convert the CSV content to bytes
+            var csvBytes = Encoding.UTF8.GetBytes(csvContent);
+
+            // Return the file as a FileResult
+            return File(csvBytes, "text/csv", "users.csv");
+        }
+
+        public IActionResult ProcessCSV(IFormFile csvFile)
+        {
+            if (csvFile != null && csvFile.Length > 0)
+            {
+                using (var reader = new StreamReader(csvFile.OpenReadStream()))
+                using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HasHeaderRecord = true,
+                }))
+                {
+                    var records = csv.GetRecords<UserImport>().ToList();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ IntegriraniSistemiH1/IntegriraniSistemiH1/Models/UserImport.cs
@@ -0,0 +1,8 @@
+﻿namespace IntegriraniSistemiH1.Models
+{
+    public class UserImport
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
